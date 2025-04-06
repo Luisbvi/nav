c2d8fd6acfe221c3835d7e79fb1ff54043d91370
Luisbvi <luisbvigamer@gmail.com>
2025-04-06 19:32:48 -0400

    account page

--- src/components/user-component.tsx
+++ src/components/user-component.tsx
@@ -166,7 +166,7 @@ const UserComponent = ({ user, role }: { user: User; role: string }) => {
 
                 <motion.li variants={itemVariants}>
                   <Link
-                    href="/profile"
+                    href="/account"
                     onClick={() => setOpen(false)}
                     className="block rounded px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                     role="menuitem"
