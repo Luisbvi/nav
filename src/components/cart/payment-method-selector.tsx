073d12a5f8e4b780ecf017394d1d00e27150ff2a
Luisbvi <luisbvigamer@gmail.com>
2025-06-27 16:47:22 -0400

    binance changes

--- src/components/cart/payment-method-selector.tsx
+++ src/components/cart/payment-method-selector.tsx
@@ -21,7 +21,7 @@ export default function PaymentMethodSelector({
     //  { id: 'credit_card', label: t('credit_card') },
     { id: 'cash', label: t('cash') },
     // { id: 'pagomovil', label: t('pagomovil') },
-    { id: 'binance', label: 'Binance' },
+    // { id: 'binance', label: 'Binance' },
   ];
 
   return (
@@ -42,7 +42,7 @@ export default function PaymentMethodSelector({
                 <Banknote className="mr-2 h-5 w-5 text-gray-600 dark:text-gray-400" />
               )}
 
-              {method.id === 'binance' && (
+              {/*method.id === 'binance' && (
                 <svg
                   className="mr-2 h-5 w-5 text-gray-600 dark:text-gray-400"
                   xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +51,7 @@ export default function PaymentMethodSelector({
                 >
                   <path d="M12 2l3.09 3.09L12 8.18l-3.09-3.09L12 2zm6.36 3.64L22 9.27l-3.64 3.64-2.73-2.73 2.73-2.73zm-12.72 0l2.73 2.73-2.73 2.73L2 9.27l3.64-3.63zM12 9.27l3.09 3.09-3.09 3.09-3.09-3.09L12 9.27zm6.36 6.36l2.73 2.73L18.36 22 15.64 19.27l2.72-2.73zm-12.72 0l2.73 2.73L5.64 22 2.91 19.27l2.73-2.73z" />
                 </svg>
-              )}
+              )*/}
               <span>{method.label}</span>
             </Label>
           </div>
